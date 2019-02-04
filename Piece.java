e7ff57421910adf72c826c3d14b1f3a9f43d11dc
ByeNow12 <33947059+ByeNow12@users.noreply.github.com>
2019-02-04 15:27:17 -0700

    Create Piece.java

---
+++ Piece.java
@@ -0,0 +1,15 @@
+public class Piece{
+
+    private final String team;
+    
+    public void calculateMoves(int[] board){
+        //calculate valid moves
+        // this function may even just be removed
+    }
+    
+    public String getTeam(){
+        return team;
+    }
+    
+    //more methods
+}
