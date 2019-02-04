18e44f34de936fdf3bc8f869881071a810e1fe47
ByeNow12 <33947059+ByeNow12@users.noreply.github.com>
2019-02-04 15:33:18 -0700

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -2,6 +2,10 @@ public class Piece{
 
     private final String team;
     
+    public Piece(String team){
+        this.team = team;
+    }
+    
     public void calculateMoves(int[] board){
         //calculate valid moves
         // this function may even just be removed
