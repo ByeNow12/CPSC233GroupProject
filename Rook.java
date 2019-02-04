14f83db4b9b524e88ac45afce3599375e2ac8334
ByeNow12 <33947059+ByeNow12@users.noreply.github.com>
2019-02-04 15:34:43 -0700

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,4 +1,9 @@
 public class Rook extends Piece{
+    
+    public Rook(String team){
+        super(team);
+    }
+    
     public void calculateMoves(int[] board){ //overwrites calculateMoves method from the superclass
         //calculate rook moves
     }
