035df88a5a3a875cac1c1437e2317186cafe9130
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-02-06 14:53:23 -0700

    Add files via upload

---
+++ Spot.java
@@ -0,0 +1,10 @@
+public class Spot { //class for the spots on board
+	
+	public boolean isOccupied() //checks if spot is occupied
+		//if spot empty return true
+		//if spot occupied, return false
+		
+	public Move() { //method that moves piece
+		//if spot empty, move piece 
+		//if spot not empty, player moves piece somewhere else
+}
