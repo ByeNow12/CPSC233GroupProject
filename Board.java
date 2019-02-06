73e4dbcec097315ef77f4fa6d8b0071305e171ab
CPSC233 <47337034+CPSC233@users.noreply.github.com>
2019-02-06 14:51:58 -0700

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,3 +1,29 @@
-public class board {
+import java.util.Arrays;
+
+public class Board {
+    private String[][] boardPositions = new String[8][8];
+
+    public Board() {
+    }
+
+    public String[][] getBoardPosition() {
+        return this.boardPositions;
+    }
+
+    public void defaultPositions() {
+        for (int i = 0; i < 8; i++) {
+            boardPositions[1][i] = "w_Pa";
+        }
+        for (int i = 0; i < 8; i++) {
+            boardPositions[7][i] = "b_Pa";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.defaultPositions();
+        System.out.println(Arrays.deepToString(b.getBoardPosition()));
+    }
 
 }
