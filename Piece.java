8cd508413fb89fc216aa3f1da2b8591d57ed8543
ByeNow12 <33947059+ByeNow12@users.noreply.github.com>
2019-02-06 15:00:32 -0700

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,7 +1,7 @@
 public class Piece{
     
-    //in calling this method oens hould already have determined the peice is a rook and it will return an array showing true for valid moves and false everyhere else
-    
+    //in calling this method one should already have determined the peice is a rook and it will return an array showing true for valid moves and false everyhere else
+    // also need to add functionality to check teams, in current state it will take a piece regardless of whether it is on the same team or not
     public static bool[][] calculateRookMoves(String[][] board, int[] pos){ 
         x = pos[0];
         y = pos[1];
@@ -45,5 +45,17 @@ public class Piece{
         return moves;
     }
     
+    public static bool[][] calculatePawnMoves(String[][] board, int[] pos, String team){
+        x = pos[0];
+        y = pos[1];
+        bool[][] moves = bool[8][8];
+        if team.equals("white"){
+            moves[x+1][y+1] = true;
+            moves[x+1][y-1] = true;
+        }
+        else{
+            moves[x-1][y+1] = true;
+            moves[x-1][y-1] = true;
+        }
     //more methods
 }
