e734ea7af0f1e771e26240c747702a3f7575a318
CPSC233 <47337034+CPSC233@users.noreply.github.com>
2019-02-24 15:20:52 -0700

    Add files via upload

--- GameConfiguration.java
+++ GameConfiguration.java
@@ -15,7 +15,41 @@ public class GameConfiguration {
 	}
 	
 	public boolean isValidMove(Move move){
-		return false; //todo actual validation logic here
+		int[] lastMove = move.getFrom();
+		int[] currentMove = move.getTo();
+		String token = board.getBoardPosition()[lastMove[0]][lastMove[1]];
+		pieceColor = token.charAt(0);
+		if (pieceColor == 'w') {
+			string pieceColorString = "white";
+		}
+		else {
+			string pieceColorString = "black";
+		}
+		pieceType = token.substring(3);
+		Piece selectedPiece = new Piece();
+		String[] pieces = {"Ro", "Kn", "Bi", "Qu", "Ki", "Pa"};
+		for (type : pieces) {
+			if (pieceType == type) {
+				if (type == "Ro") {
+					return selectedPiece.calculateRookMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+				else if (type.equals("Kn")) {
+					return selectedPiece.calculateKnightMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+				else if (type.equals("Bi")) {
+					return selectedPiece.calculateBishopMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+				else if (type.equals("Qu")) {
+					return selectedPiece.calculateQueenMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+				else if (type.equals("Ki")) {
+					return selectedPiece.calculateKingMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+				else {
+					return selectedPiece.calculatePawnMoves(board, lastMove, pieceColorString)[currentMove[0]][currentMove[1]];
+				}
+			}
+		}
 	}
 	
 	public boolean hasWon(String token){
