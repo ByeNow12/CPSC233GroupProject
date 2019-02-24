6f01d66d8024d71035a5f89f0c1b4ac34fd7f49a
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-02-24 01:07:29 -0700

    Update GameConfiguration.java

--- GameConfiguration.java
+++ GameConfiguration.java
@@ -6,16 +6,16 @@ public class GameConfiguration {
 		// to do later
 	}
 	
-	public boolean isValidMove(Move move){
+	public boolean isValidMove(Move move){ //checks if the argument is a valid move
 		return false; //todo actual validation logic here
 	}
 	
-	public boolean hasWon(String token){
+	public boolean hasWon(String token){ //checks if any player has won yet 
 		//todo later
 		return false;
 	}
 	
-	public Move[] getAllValidMoves(String token){
+	public Move[] getAllValidMoves(String token){ //gets all the possible valid moves of a piece of a certain player
 		//todo later
 		return new Move[0];
 	}
