b072e37e961cf2784b725ee1f1eb2fb29aecd8dc
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-02-24 16:08:35 -0700

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,11 +1,11 @@
 ///Code does not yet compile/incomplete
-public class ComputerPlayer extends Game {
+public class ComputerPlayer {
 	private String token;
-	private Move lastMove; //unsure what this is referring to?
+	private Move lastMove; //computer considers the last move made by human player to make its move, used when AI developed
+	//lastMove will be left alone for now
 
-  //Constructors
-	public ComputerPlayer(String aToken, Move theLastMove) { //default
- 		super();
+  	//Constructors
+	public ComputerPlayer(String aToken) { //default
     		this.token = aToken;
  	 }
   
@@ -14,29 +14,26 @@ public class ComputerPlayer extends Game {
 		this.token = toCopy.token;
     		this.lastMove = toCopy.lastMove;
   	}
-  
+  	
  	/** Getter method for getting the move for the computer player
   	* @param currentConfig, 
   	* @return a random valid move until we implement the AI
  	 */
   	public Move getMove(GameConfiguration currentConfig) {
-		return lastMove; 
+		return compMove; //obtain move based on values from the setter
  	}
   
   	/** Setter method to make a random valid move for the computer
  	 * @param rowNum, the row number of the piece the computer wants to move
 	 * @param colNum, the column number of the piece the computer wants to move
   	*/
-  	public void setMove(Move rowNum, Move colNum ) {
-    		Piece p1 = new Piece(); //randomly select one existing chess piece on the board
-    		p1.CalculatePieceMoves("newPiece", newPosition, "newTeam"); //compute all legal destinations in which that piece could move
-    		p1.getAllValidMoves(tokenToMove);
-    		p1.isValidMove(tokenToMove); 
-    		
-		if (p1.isValidMove(move) != true) { //check if move is valid
-      			Move movePiece = new Move();
-			movePiece.getTo(); //if move valid, call getTo method from Move class to move piece to row and column number
-			//Randomly select one of the computed legal destinations to move the piece to
+  	public void setMove(int rowNum, int colNum) {
+		//while the row/column numbers in range of 0-8, Move class called to make a move
+		while ((rowNum && colNum) >= 0) && ((rowNum && colNum) <= 8) { 
+			Move compMove = new Move(rowNum, colNum); //call Move class to make a move based on the row/column numbers of piece 
+			if (compMove.isValidMove() == true) { //check if move is valid
+				compMove.getTo(); //if valid, move piece to desired row and column number 
+			}
 		}
     	}
 	
