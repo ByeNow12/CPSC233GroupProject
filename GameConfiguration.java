dc2008b8b01cfabd25550b8e9ec0a033a134d7f8
CPSC233 <47337034+CPSC233@users.noreply.github.com>
2019-02-24 14:43:40 -0700

    Implementing the methods of the class

--- GameConfiguration.java
+++ GameConfiguration.java
@@ -2,8 +2,16 @@ public class GameConfiguration {
 	
 	private Board board = new Board();
 	
+	public Board getBoard() {
+		return board;
+	}
+	
 	public void update(Move move){
-		// to do later
+		int[] lastMove = move.getFrom();
+		int[] currentMove = move.getTo();
+		String token = board.getBoardPosition()[lastMove[0]][lastMove[1]];
+		board.setBoardPositions(lastMove[0], lastMove[1], "0");
+		board.setBoardPositions(currentMove[0], currentMove[1], token);
 	}
 	
 	public boolean isValidMove(Move move){
@@ -18,6 +26,16 @@ public class GameConfiguration {
 	public Move[] getAllValidMoves(String token){
 		//todo later
 		return new Move[0];
+	}
+	
+	public static void main(String[] args) {
+		GameConfiguration config = new GameConfiguration();
+		Move m = new Move("t", 0, 0, 5, 5);
+		Board b = config.getBoard();
+		b.defaultPositions();
+		b.draw();
+		config.update(m);
+		b.draw();
 	}
 	
 	
