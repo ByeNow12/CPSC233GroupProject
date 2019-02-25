5d0ee8347d17a42b82ab65c657dc5dda4329121c
Riley <rtwj12@gmail.com>
2019-02-25 06:08:24 -0700

    It works! Workig on win condition

--- Game.java
+++ Game.java
@@ -5,22 +5,23 @@ class Game{
   private HumanPlayer human1;
   private HumanPlayer human2;  // May go unused in a particular instance, where a human plays against a computer.
   private ComputerPlayer ai;
-  private GameConfiguration config = new GameConfiguration();
+  private GameConfiguration config;
 
   private Scanner m = new Scanner(System.in); // Scanner for getting input regarding move selection.
   private int fR, fC, tR, tC, turnCounter;  // Always used, the first four are for move selections.
   private String c;
   
   public void setup(){
+    config = new GameConfiguration();
     Scanner format = new Scanner(System.in);
     System.out.println("Would you like to play a [H]uman or [C]omputer?");
     c = format.next();
 
-    if (c == "H"){
+    if (c.equals("H")){
       human1 = new HumanPlayer("White");
       human2 = new HumanPlayer("Black");
     }
-    if (c == "C"){
+    if (c.equals("C")){
       human1 = new HumanPlayer("White"); // Default's the human to the slightly more advantageous side, may change in the future.
       ai = new ComputerPlayer("Black"); // Unsure of the purpose of the instance variable of Move type upon initialisation.
     }
@@ -72,17 +73,16 @@ class Game{
 
   public void play(){
     int turnCounter = 0;
-    config.getBoard().draw();
     while (!config.hasWon('w') || !config.hasWon('b')){
+      config.getBoard().draw();
       if (turnCounter % 2 == 0)
         whitePlay();
-      if ((turnCounter % 2 == 1) && (c == "H"))
+      if ((turnCounter % 2 == 1) && (c.equals("H")))
        blackPlay();
-      if ((turnCounter % 2 == 1) && (c == "C"))
+      if ((turnCounter % 2 == 1) && (c.equals("C")))
        config.update(ai.getMove(config)); // Here I assume the ComputerPlayer cannot make invalid moves.
       // A method to check for draws/stalemates must be added to the GameConfiguration class, and incited here.
       turnCounter++;
-	config.getBoard().draw();
       }
   }
   public static void main(String[] args) {
