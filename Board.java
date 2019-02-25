1c7efab0a8b1d070956844e7da0003481d28f24f
CPSC233 <47337034+CPSC233@users.noreply.github.com>
2019-02-25 15:22:58 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Board {
     private String[][] boardPositions = new String[8][8];
 
+	//Constructor
     public Board() {
         for (int i = 0; i < 8; i++) {
             for (int x = 0; x < 8; x++) {
@@ -11,14 +12,27 @@ public class Board {
         }
     }
 
+	/**
+	* a getter method for the boardPositions array
+	* @return: boardPositions: String[][]
+	*/
     public String[][] getBoardPosition() {
         return this.boardPositions;
     }
 
+	/**
+	* sets an index of the boardPositions varible to a particular value
+	* @param: i: int, first index
+	* @param: x: int, second index
+	* @param: value: String, value to change to
+	*/
     public void setBoardPositions(int i, int x, String value) {
         this.boardPositions[i][x] = value;
     }
 
+	/**
+	* Sets the defualt positions of all the pieces on the boardPositions array.
+	*/
     public void defaultPositions() {
         String[] pieces = {"w_Ro", "w_Kn", "w_Bi", "b_Ro", "b_Kn", "b_Bi"};
         for (int i = 0; i < 8; i++) {
@@ -39,6 +53,9 @@ public class Board {
         boardPositions[7][3] = "b_Qu";
     }
 
+	/**
+	* Draws boardPositions to the screen according to the value each index contains.
+	*/
     public void draw() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
