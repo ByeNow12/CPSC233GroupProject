3164b418af4f726c48817e63d4feee5cd780d5e8
Riley <rtwj12@gmail.com>
2019-02-25 06:12:35 -0700

    It works!

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ComputerPlayer {
 	private String token;
 	private Move lastMove; //computer considers the last move made by human player to make its move, used when AI developed
@@ -19,7 +21,7 @@ public class ComputerPlayer {
   	* @return a random valid move until we implement the AI
  	*/
   	public Move getMove(GameConfiguration currentConfig) {
-		return lastMove; //obtain move based on values from the setter
+		return getRandomMove(currentConfig); //obtain move based on values from the setter
  	}
   
   	/** Setter method to make a random valid move for the computer player
@@ -30,13 +32,29 @@ public class ComputerPlayer {
   	*/
   	public void setMove(int fromRowNum, int fromColNum, int toRowNum, int toColNum) {
 		//while the row/column numbers in range of 0-8, Move class called to make a move
-		while ((toRowNum >= 0 && toColNum >= 0) && (toRowNum <= 8 && toColNum <= 8)) {
+		while ((toRowNum >= 0 && toColNum >= 0) && (toRowNum < 8 && toColNum < 8)) {
 			Move compMove = new Move(token, fromRowNum, fromColNum, toRowNum, toColNum); //call Move class to make a move based on the row/column number 
 			if (config.isValidMove(compMove) == true) { //check if move is valid
 				compMove.getTo(); //if valid, move piece to desired row and column number 
 			}
 		}
     	}
+
+	public Move getRandomMove(GameConfiguration config){ //literally just brute forces until a valid move comes out
+		Random r = new Random();
+		int[] nums = new int[4];
+		Move move;
+	
+		do {   //do-while loops are convenient
+			for (int i = 0; i < 4; i++){
+				nums[i] = r.nextInt(8); //random int (0,8]
+			}
+			move = new Move("Black", nums[0], nums[1], nums[2], nums[3]);
+		} while (!config.isValidMove(move) || config.getBoard().getBoardPosition()[nums[0]][nums[1]].charAt(0) == 'w'); //also makes sure it's not about too move a white piece
+
+		return move;
+	}
+		
 	
  	public static void main(String[] args) {
 		ComputerPlayer p2 = new ComputerPlayer("black");
