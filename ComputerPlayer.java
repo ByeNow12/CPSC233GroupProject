7c0f6257abfb6567ebccf125dafe4a56180aebfc
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-02-24 20:13:49 -0700

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,4 +1,3 @@
-///Code does not yet compile/incomplete
 public class ComputerPlayer {
 	private String token;
 	private Move lastMove; //computer considers the last move made by human player to make its move, used when AI developed
@@ -10,27 +9,28 @@ public class ComputerPlayer {
  	 }
   
 	public ComputerPlayer(ComputerPlayer toCopy) { //copy constructor
-   		super(toCopy);
 		this.token = toCopy.token;
     		this.lastMove = toCopy.lastMove;
   	}
   	
  	/** Getter method for getting the move for the computer player
-  	* @param currentConfig, 
+  	* @param currentConfig,
   	* @return a random valid move until we implement the AI
- 	 */
+ 	*/
   	public Move getMove(GameConfiguration currentConfig) {
 		return compMove; //obtain move based on values from the setter
  	}
   
-  	/** Setter method to make a random valid move for the computer
- 	 * @param rowNum, the row number of the piece the computer wants to move
-	 * @param colNum, the column number of the piece the computer wants to move
+  	/** Setter method to make a random valid move for the computer player
+ 	* @param fromRowNum, the row number of the piece the computer wants to move from
+	* @param fromColNum, the column number of the piece the computer wants to move from
+	* @param toRowNum, the row number the computer wants to move to 
+	* @param toColNum, the column number the computer wants to move to 
   	*/
-  	public void setMove(int rowNum, int colNum) {
+  	public void setMove(int fromRowNum, int fromColNum, int toRowNum, int toColNum) {
 		//while the row/column numbers in range of 0-8, Move class called to make a move
-		while ((rowNum >= 0 && colNum >= 0) && (rowNum <= 8 && colNum <= 8)) { 
-			Move compMove = new Move(rowNum, colNum); //call Move class to make a move based on the row/column numbers of piece 
+		while ((toRowNum >= 0 && toColNum >= 0) && (toRowNum <= 8 && toColNum <= 8)) {
+			Move compMove = new Move(token, fromRowNum, fromColNum, toRowNum, toColNum); //call Move class to make a move based on the row/column number 
 			if (compMove.isValidMove() == true) { //check if move is valid
 				compMove.getTo(); //if valid, move piece to desired row and column number 
 			}
