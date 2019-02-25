250cafcfc5d405f1d2937cd56cda3b90afb551b7
Riley <rtwj12@gmail.com>
2019-02-25 06:13:02 -0700

    It works!

--- Piece.java
+++ Piece.java
@@ -1,12 +1,11 @@
 
 public class Piece{
-    
+
     //in calling this method one should already have determined the peice is a rook and it will return an array showing true for valid moves and false everyhere else
-    // also need to add functionality to check teams, in current state it will take a piece regardless of whether it is on the same team or not
-    public static Boolean[][] calculateRookMoves(String[][] board, int[] pos, char team){ 
+    public static boolean[][] calculateRookMoves(String[][] board, int[] pos, char team){ 
         int x = pos[0];
         int y = pos[1];
-        Boolean[][] moves = new Boolean[8][8];
+        boolean[][] moves = new boolean[8][8];
         for (int i = x-1; i >= 0; i--){
             if (team == board[i][y].charAt(0)) break;
             if (board[i][y].equals("0")){
@@ -50,25 +49,35 @@ public class Piece{
         return moves;
     }
     
-    public static Boolean[][] calculatePawnMoves(String[][] board, int[] pos, char team){
-        int x = pos[0];
-        int y = pos[1];
-        Boolean[][] moves = new Boolean[8][8];
-        if (team == 'w'){ //assuming white is on the top of the board
-            moves = safeCheck(board, new int[]{x-1, y+1}, team, moves);
-            moves = safeCheck(board, new int[]{x+1, y+1}, team, moves);
+    public static boolean[][] calculatePawnMoves(String[][] board, int[] pos, char team){
+        int r = pos[0];
+        int c = pos[1];
+        boolean[][] moves = new boolean[8][8];
+        if (team == 'w' && r < 7){ //assuming white is on the top of the board
+            if (c < 7){
+		if ('b' == board[r+1][c+1].charAt(0)) moves[r+1][c+1] = true;
+	    }
+	    if (c > 1){
+		if ('b' == board[r+1][c-1].charAt(0)) moves[r+1][c-1] = true;
+	    }
+	    if ('0' == board[r+1][c].charAt(0)) moves[r+1][c] = true;
         }
-        else{
-            moves = safeCheck(board, new int[]{x-1, y-1}, team, moves);
-            moves = safeCheck(board, new int[]{x+1, y-1}, team, moves);
+        else if (r > 1){
+            if (c < 7){
+		if ('w' == board[r-1][c+1].charAt(0)) moves[r-1][c+1] = true;
+	    }
+            if (c > 1){
+		if ('w' == board[r-1][c-1].charAt(0)) moves[r-1][c-1] = true;
+	    }
+	    if ('0' == board[r-1][c].charAt(0)) moves[r-1][c] = true;
         }
 	return moves;
     }
     
-    public static Boolean[][] calculateKnightMoves(String[][] board, int[] pos, char team){
+    public static boolean[][] calculateKnightMoves(String[][] board, int[] pos, char team){
         int x = pos[0];
         int y = pos[1];
-        Boolean[][] moves = new Boolean[8][8];
+        boolean[][] moves = new boolean[8][8];
         moves = safeCheck(board, new int[]{x+2, y+1}, team, moves);
         moves = safeCheck(board, new int[]{x+1, y+2}, team, moves);
         moves = safeCheck(board, new int[]{x-1, y+2}, team, moves);
@@ -80,10 +89,10 @@ public class Piece{
 	return moves;
     }
 
-    public static Boolean[][] calculateBishopMoves(String[][] board, int[] pos, char team){
+    public static boolean[][] calculateBishopMoves(String[][] board, int[] pos, char team){
 	int x = pos[0];
         int y = pos[1];
-	Boolean[][] moves = new Boolean[8][8];
+	boolean[][] moves = new boolean[8][8];
 	for (int i = x+1, j = y+1; i < 8 && j < 8; i++, j++){
 		if (team == board[i][j].charAt(0)) break;
 		moves[i][j] = true;
@@ -115,10 +124,10 @@ public class Piece{
 	return moves;
     }
 	
-    public static Boolean[][] calculateKingMoves(String[][] board, int[] pos, char team){
+    public static boolean[][] calculateKingMoves(String[][] board, int[] pos, char team){
 	int x = pos[0];
         int y = pos[1];
-	Boolean[][] moves = new Boolean[8][8];
+	boolean[][] moves = new boolean[8][8];
 	moves = safeCheck(board, new int[]{x+1, y+1}, team, moves);
 	moves = safeCheck(board, new int[]{x+1, y}, team, moves);
 	moves = safeCheck(board, new int[]{x+1, y-1}, team, moves);
@@ -130,10 +139,10 @@ public class Piece{
 	return moves;
     }
 	
-    public static Boolean[][] calculateQueenMoves(String[][] board, int[] pos, char team){
-	Boolean[][] moves = new Boolean[8][8];
-	Boolean[][] rMoves = calculateRookMoves(board, pos, team);
-	Boolean[][] bMoves = calculateBishopMoves(board, pos, team);
+    public static boolean[][] calculateQueenMoves(String[][] board, int[] pos, char team){
+	boolean[][] moves = new boolean[8][8];
+	boolean[][] rMoves = calculateRookMoves(board, pos, team);
+	boolean[][] bMoves = calculateBishopMoves(board, pos, team);
 	for (int i = 0; i < 8; i++){
 		for (int j = 0; j < 8; j++){
 			moves[i][j] = rMoves[i][j] || bMoves[i][j];
@@ -141,10 +150,10 @@ public class Piece{
 	}
 	return moves;
     }
-    private static Boolean[][] safeCheck(String[][] board, int[] pos, char team, Boolean[][] moves){
+    private static boolean[][] safeCheck(String[][] board, int[] pos, char team, boolean[][] moves){
 	int x = pos[0];
         int y = pos[1];
-	if (x > -1 && x < 8 && y > -1 && x < 8 && !(team == board[x][y].charAt(0))){
+	if (x > -1 && x < 8 && y > -1 && y < 8 && !(team == board[x][y].charAt(0))){
 		moves[x][y] = true;
 	}
 	return moves;
