9d3df73e278dab1720f0dbe4309761dda4f72030
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-03-06 17:40:55 -0700

    Update HumanPlayer.java

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+* 2019-03-06
+* Author: Carmen
+* Class that allows human players to make a move and contains data related to the player 
+* Methods allow for interaction with the users, validate input and return the data entered by the user as a Move object
+*/
 public class HumanPlayer {
 
 	private String token;
