1c8003b3db586ee698a2d185c87afa7c523415e8
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-03-09 19:45:55 -0700

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,9 +1,15 @@
+/**
+* 2019-03-09
+* Authors: Riley
+* Class calculates moves for each type of piece (pawn, rook, knight, bishop, queen, king) based on what moves the pieces are allowed to make according to standard chess game rules
+* Returns all the possible moves for each piece
+*/
 public class Piece {
-    /** Method for calculating possible moves for rook piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for rook piece based on what moves it is allowed to make
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the rook piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of rook and false everywhere else
     */ 
     public static boolean[][] calculateRookMoves(String[][] board, int[] pos, char team){
@@ -53,13 +59,13 @@ public class Piece {
         return moves;
     }
     
-    /** Method for calculating possible moves for pawn piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for pawn piece based on what moves it is allowed to make 
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
 	* Updated so that the first move of a pawn is able to move forward one or two spaces
 	* @version 2.0
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the pawn piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of pawn and false everywhere else
     */
     public static boolean[][] calculatePawnMoves(String[][] board, int[] pos, char team){
@@ -135,11 +141,11 @@ public class Piece {
 		return moves;
     }
 
-    /** Method for calculating possible moves for knight piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for knight piece based on what moves it is allowed to make 
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the knight piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of knight and false everywhere else
     */
     public static boolean[][] calculateKnightMoves(String[][] board, int[] pos, char team){
@@ -157,11 +163,11 @@ public class Piece {
 	return moves;
     }
 
-    /** Method for calculating possible moves for bishop piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for bishop piece based on what moves it is allowed to make 
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the bishop piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of bishop and false everywhere else
     */
     public static boolean[][] calculateBishopMoves(String[][] board, int[] pos, char team){
@@ -199,11 +205,11 @@ public class Piece {
 	return moves;
     }
 
-    /** Method for calculating possible moves for king piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for king piece based on what moves it is allowed to make 
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the king piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of king and false everywhere else
     */
     public static boolean[][] calculateKingMoves(String[][] board, int[] pos, char team){
@@ -221,11 +227,11 @@ public class Piece {
 	return moves;
     }
 
-    /** method for calculating possible moves for queen piece based on what moves the pieces are allowed to make according to standard chess rules
+    /** Method for calculating possible moves for queen piece based on what moves it is allowed to make 
     * Creates a 8 by 8 board with valid moves marked as true and invalid moves marked as false
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the queen piece
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @return an array that shows true for valid moves of queen and false everywhere else
     */
     public static boolean[][] calculateQueenMoves(String[][] board, int[] pos, char team){
@@ -243,7 +249,7 @@ public class Piece {
     /** Method for ensuring that the pieces move within the 8 by 8 board  
     * @param String[][] board, an arraylist for the board
     * @param int[] pos, array that contains index for position of the pieces
-    * @param char team, a character for the team 
+    * @param char team, a character for the team playing
     * @param boolean[][] moves, an array that shows if the moves are true
     * @return moves, an array that shows true if the pieces move within the board range
     */
