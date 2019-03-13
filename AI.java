8ea5982f8335f68f654fb49e4c5410428f0259e5
ByeNow12 <33947059+ByeNow12@users.noreply.github.com>
2019-03-13 14:44:02 -0600

    Add files via upload

---
+++ AI.java
@@ -0,0 +1,24 @@
+/**
+* 2019-03-06
+* Author: Riley
+* Words about class
+*/
+public class AI extends ComputerPlayer {
+
+	String[][] currentBoard;
+	String[][][] hBoards;
+
+ 	/** Getter method for getting the move for the computer player
+  	* @param
+  	* @return 
+ 	*/
+ 	@Override
+  	public Move getMove(GameConfiguration currentConfig) {
+		
+ 	}
+  
+ 	public static void main(String[] args) {
+
+	}
+}
+
