b2e61027a55e877d0715a1e45616b0bd1f44e207
ckhuu <carmen.khuu@gmail.com>
2019-03-20 14:24:06 -0600

    Add files via upload
    added start method

--- Draw.java
+++ Draw.java
@@ -159,4 +159,35 @@ public class Draw extends Application {
 		}
 	}
 	
+	public void start(Stage primaryStage) throws FileNotFoundException {
+		//Image instance created, passing FileInputStream as parameter to the Image to load the image 
+		Image boardImage = new Image(new FileInputStream("Chessboard.png")); //parameter is the image file path
+		//file path to image depends on where you save board image
+		
+		//ImageView instance created, passing Image instance as parameter
+		ImageView boardImageView = new ImageView(boardImage);
+		
+		//BorderPane to center the chess board image
+		BorderPane pane = new BorderPane();
+		//wrap for the center of the border pane
+		wrap = new StackPane();
+		//pane that handle all event handling
+		eventPane = new Pane();
+		wrap.setMaxSize(400, 400);
+		wrap.getChildren().add(boardImageView);
+		wrap.getChildren().add(eventPane);
+		wrap.getChildren().add(piecePane);
+		pane.setCenter(wrap);
+		
+		config = new GameConfiguration();
+		config.getBoard().defaultPositions();
+		
+		Scene scene = new Scene(pane, 450, 500); //window size is 450 by 500 pixels
+		//tie the mouse event to the wrapping pane. The team must be specified
+		wrap.setOnMouseClicked(new ClickHandle(this, config, 'w'));
+		primaryStage.setTitle("Chess Game"); //set title to stage
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		draw();
+	}
 }
