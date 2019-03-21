da42a4dfd647bd0aa16e47d69a52c1b6ca8acab7
shavonnetran <47336813+shavonnetran@users.noreply.github.com>
2019-03-20 21:37:06 -0600

    Update and rename Board.java to logic_package/Board.java

--- logic_package/Board.java
+++ logic_package/Board.java
@@ -1,3 +1,5 @@
+package logic_package;
+
 import java.util.Arrays;
 
 /**
@@ -9,7 +11,7 @@ import java.util.Arrays;
 public class Board {
     private String[][] boardPositions = new String[8][8];
 
-	//Constructor
+    //Constructor
     public Board() {
         for (int i = 0; i < 8; i++) {
             for (int x = 0; x < 8; x++) {
@@ -18,31 +20,37 @@ public class Board {
         }
     }
 
-	/**
-	* a getter method for the boardPositions array
-	* @return: boardPositions: String[][]
-	*/
+    /**
+    * Getter method for the boardPositions array
+    * @return boardPositions, the positions for the board in String[][] format
+    */
     public String[][] getBoardPosition() {
         return this.boardPositions;
     }
-	
-     public String[][] getBoardPositionPieceInfo(int i, int x) {
+
+    /**
+    * Getter for the 
+    * @param i, int: first index
+    * @param x, int: second index
+    * @return boardPositions[i][x], the positions for the board at index i and index x
+    */
+    public String[][] getBoardPositionPieceInfo(int i, int x) {
         return this.boardPositions[i][x];
     }
 
-	/**
-	* sets an index of the boardPositions varible to a particular value
-	* @param: i: int, first index
-	* @param: x: int, second index
-	* @param: value: String, value to change to
-	*/
+    /**
+    * Sets an index of the boardPositions varible to a particular value
+    * @param i, int: first index
+    * @param x, int: second index
+    * @param value, String: value to change to
+    */
     public void setBoardPositions(int i, int x, String value) {
         this.boardPositions[i][x] = value;
     }
 
-	/**
-	* Sets the defualt positions of all the pieces on the boardPositions array.
-	*/
+    /**
+    * Sets the defualt positions of all the pieces on the boardPositions array
+    */
     public void defaultPositions() {
 	String[] pieces = {"w_Ro", "w_Kn", "w_Bi", "b_Ro", "b_Kn", "b_Bi"};
 	
@@ -64,9 +72,9 @@ public class Board {
         boardPositions[7][3] = "b_Qu";
     }
 
-	/**
-	* Draws boardPositions to the screen according to the value each index contains.
-	*/
+    /**
+    * Draws boardPositions to the screen according to the value each index contains
+    */
     public void draw() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
