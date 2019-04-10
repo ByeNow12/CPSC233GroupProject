bb540dcba3749765d30641f8c42aa112706ac689
Riley James <riley.james1@ucalgary.ca>
2019-04-10 13:57:10 -0600

    Some progress and compiles now

--- logic_package/AI.java
+++ logic_package/AI.java
@@ -29,13 +29,15 @@ public class AI extends ComputerPlayer {
 	*Calculates the most advantageous capture the piece in the specified position can make - assumes errors handled before method call
 	*@param pos - the position of the piece for which you want to calculate the best move
 	*@param board - the current game board
-	*@return an array with the difference in points values between the piece and the piece it can take and the position of that piece, in the form {diff, row, col}
+	*@return an array with the difference in points values between the piece and the piece it can 
+	*take and the position of that piece, in the form {diff, row, col}, 
+	*returns position as -1,-1 if no valid captures
   	*/
 
 	private int[] getBestCapture(int[] pos, String[][] board){
 
-		int pieceValue;
-		int takenValue;
+		int pieceValue = 0;
+		int takenValue = 0;
 		int value;
 		boolean[][] validMoves;
 		int[] move;
@@ -65,16 +67,19 @@ public class AI extends ComputerPlayer {
 			validMoves = Piece.calculateKingMoves(board, pos, team);
 			pieceValue = Piece.KING_VALUE;
 		}
-
-		for (int row = 0; row < 8; i++){
-			for (int col = 0; col < 8; j++){
-				if (!validMoves[i][j]) continue;	//if it's not a valid move, skip it
+		else{
+			return new int[] {0,-1,-1};
+		}
+		move = new int[] {-1,-1};
+		for (int row = 0; row < 8; row++){
+			for (int col = 0; col < 8; col++){
+				if (!validMoves[row][col]) continue;	//if it's not a valid move, skip it
 
 				char pTeam = board[row][col].charAt(0);
-				if (pTeam == '0' || pTeam == team) continue;//skips if empty square (not yet being considered) or if position is already occupied by friendly piece
+				if (pTeam == '0' /*|| pTeam == team*/) continue;//skips if empty square (not yet being considered) or if position is already occupied by friendly piece
 
-				String pType = board[row][col].substring(2);
-				int pValue;
+				String tType = board[row][col].substring(2);
+				int pValue = 0;
 
 				if (tType.equals("Pa")) pValue = Piece.PAWN_VALUE;
 				else if (tType.equals("Bi")) pValue = Piece.BISHOP_VALUE;
@@ -91,7 +96,58 @@ public class AI extends ComputerPlayer {
 		}
 		value = takenValue - pieceValue;
 		return new int[] {value, move[0], move[1]};
-}
+	}
+	
+	public int[] getBestSpaceMove(int[] pos, String[][] board){
+		
+		boolean[][] validMoves;
+		int[] move;
+		String type = board[pos[0]][pos[1]].substring(2);
+		
+		if (type.equals("Pa")){
+			validMoves = Piece.calculatePawnMoves(board, pos, team);
+		}
+		else if (type.equals("Bi")){
+			validMoves = Piece.calculateBishopMoves(board, pos, team);
+		}
+		else if (type.equals("Kn")){
+			validMoves = Piece.calculateKnightMoves(board, pos, team);
+		}
+		else if (type.equals("Ro")){
+			validMoves = Piece.calculateRookMoves(board, pos, team);
+		}
+		else if (type.equals("Qu")){
+			validMoves = Piece.calculateQueenMoves(board, pos, team);
+		}
+		else if (type.equals("Ki")){
+			validMoves = Piece.calculateKingMoves(board, pos, team);
+		}
+		else{
+			return new int[] {-1,-1};
+		}
+		
+		move = new int[] {-1,-1};
+		
+		for (int row = 2; row < 6; row++){
+			for (int col = 2; col < 6; col++){
+				//will aim for the center of the board, and get the closest move it can to the very center
+				if (!validMoves[row][col]) continue;
+				
+				if ((row < 5 && row > 2)&&(col < 5 && col > 2)){
+					move = new int[] {row,col};
+					continue;
+				}
+				
+				if ((row < 5 && row > 2)||(col < 5 && col > 2)){
+					move = new int[] {row,col};
+					continue;
+				}
+				move = new int[] {row,col};
+			}
+		}
+		
+		return move;
+	}
 
  	public static void main(String[] args) {
 
