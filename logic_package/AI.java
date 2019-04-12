14c619c5196b95425a58405af9fff8de47ca4939
CPSC233 <47337034+CPSC233@users.noreply.github.com>
2019-04-12 02:20:18 -0600

    Fixed kingPos initialization problem.

--- logic_package/AI.java
+++ logic_package/AI.java
@@ -39,7 +39,7 @@ public class AI extends ComputerPlayer {
 
 		if (currentConfig.isCheck(team)){
 			//first checks if king can move out of check
-			int[] kingPos;
+			int[] kingPos = {0, 0};
 			boolean[][] validMoves;
 			for (int i = 0; i < pieces.size(); i++){
 				int[] pPos = pieces.get(i);
